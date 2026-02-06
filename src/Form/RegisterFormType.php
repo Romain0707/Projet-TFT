439c137af25d6d6de87f3c66795a012dcb29f74e
Mylan <mylanbrasierpro@gmial.com>
2026-02-06 13:24:49 +0100

    start register page

--- src/Form/RegisterFormType.php
+++ src/Form/RegisterFormType.php
@@ -6,6 +6,7 @@ use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 
 class RegisterFormType extends AbstractType
 {
@@ -14,7 +15,21 @@ class RegisterFormType extends AbstractType
         $builder
             ->add('username')
             ->add('password')
-        ;
+            ->add('username', TextType::class, [
+                'attr' => [
+                    'class' => 'form-input',
+                    'placeholder' => 'Pseudonyme'
+                ],
+                'label' => false
+            ])
+            ->add('password', TextType::class, [
+                'attr' => [
+                    'class' => 'form-input',
+                    'placeholder' => 'Mots de passe'
+                ],
+                'label' => false
+            ])
+            ;
     }
 
     public function configureOptions(OptionsResolver $resolver): void
